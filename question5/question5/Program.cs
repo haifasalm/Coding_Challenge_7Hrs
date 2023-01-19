8a76a5ba237e11debbeda740d7a8c3ca1e2d9aca
haifasalm <120713613+haifasalm@users.noreply.github.com>
2023-01-19 15:22:10 +0530

    Add files via upload

---
+++ question5/question5/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("enter the number of odd numbers: ");
+int odd = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the number of even number: ");
+int even = Convert.ToInt32(Console.ReadLine());
+int n = odd + even;
+
+odd = 0;
+even = 1;
+double fare = 0;
+
+double[] fares = new double[n];
+Console.WriteLine("Enter the fares :");
+for (int i = 0; i < n; i++)
+{
+    fare = Convert.ToDouble(Console.ReadLine());
+    if (fare % 2 == 0)
+    {
+        fares[even] = fare;
+        even += 2;
+    }
+    else
+    {
+        fares[odd] = fare;
+        odd += 2;
+    }
+}
+Console.WriteLine("Entered fares are");
+foreach (int i in fares)
+{
+    Console.WriteLine(i);
+}
