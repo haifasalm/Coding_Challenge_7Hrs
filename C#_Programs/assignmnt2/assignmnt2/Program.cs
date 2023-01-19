ff58890fe2beaf625d91cb5eed2088a9dcda7be2
haifasalm <120713613+haifasalm@users.noreply.github.com>
2023-01-19 14:10:38 +0530

    Add files via upload

---
+++ C#_Programs/assignmnt2/assignmnt2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Transactions;
+
+Console.WriteLine("enter the flight fare: ");
+double fare = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter the time: ");
+int time = Convert.ToInt32(Console.ReadLine());
+
+if(time >= 6 && time < 9)
+{
+    fare = (fare * .1) + fare;
+    Console.WriteLine(fare);
+}
+else if(time >= 9 && time < 17)
+{
+    fare = (fare * .2) + fare;
+    Console.WriteLine(fare);
+}
+else if (time >= 17 && time < 23)
+{
+    fare = (fare * .07) + fare;
+    Console.WriteLine(fare);
+}
+else
+{
+    fare = (fare * .05) + fare;
+    Console.WriteLine(fare);
+}
+
