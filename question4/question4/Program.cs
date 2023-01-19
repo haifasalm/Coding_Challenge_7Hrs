a7eeb0088622e9c5cac489c89e4bdd6c160b4402
haifasalm <120713613+haifasalm@users.noreply.github.com>
2023-01-19 15:21:06 +0530

    Add files via upload

---
+++ question4/question4/Program.cs
@@ -0,0 +1,20 @@
+﻿DateTime dob;
+Console.WriteLine("enter the date of birth: ");
+dob = Convert.ToDateTime(Console.ReadLine());
+DateTime curr;
+curr= DateTime.Now;
+TimeSpan ts = curr - dob;
+Console.WriteLine("age in days: " + ts.Days);
+Console.WriteLine("age in hours: " + ts.Days * 24);
+Console.WriteLine("age in weeks: " + ts.Days / 7);
+Console.WriteLine("age in months: " + ts.Days / 30);
+Console.WriteLine("age in years: " + ts.Days / 365);
+int year = dob.Year;
+if(year % 4 == 0)
+{
+    Console.WriteLine("year is a leap year");
+}
+else
+{
+    Console.WriteLine("not a leap year");
+}
