db81c00ff9e7a042e052714bfeb128a2cb61ba03
haifasalm <120713613+haifasalm@users.noreply.github.com>
2023-01-20 12:28:20 +0530

    Add files via upload

---
+++ question9/question9/Program.cs
@@ -0,0 +1,33 @@
+﻿DateTime dob;
+int[] ages = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("enter the dob");
+    dob = Convert.ToDateTime(Console.ReadLine());
+    DateTime curr;
+    curr = DateTime.Now;
+    TimeSpan diff = curr - dob;
+    int age = diff.Days / 365;
+    ages[i] = age;
+
+}
+foreach (var item in ages)
+{
+    if(item < 10)
+    {
+        Console.WriteLine("person is kid");
+    }
+    else if(item >= 10 & item <30)
+    {
+        Console.WriteLine("person is youth");
+    }
+    else if(item >= 30 & item < 60)
+    {
+        Console.WriteLine("person is adult");
+    }
+    else
+    {
+        Console.WriteLine("person is older");
+    }
+}
+    
